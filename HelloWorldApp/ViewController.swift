a8c1b9cc19b837735d0d0b50bf1cbc68cb59807b
cgurinq <70450625+cgurinq@users.noreply.github.com>
2021-12-13 05:02:47 +0200

    Add logic for button

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var helloWorldLable: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloWorldLable.isHidden = true
+        startButton.layer.cornerRadius = 10
+        helloWorldLable.textColor = .systemRed
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func startPressed() {
+        helloWorldLable.isHidden.toggle()
+        if helloWorldLable.isHidden {
+            startButton.setTitle("Show text", for: .normal)
+        }
+        else {
+            startButton.setTitle("Hide text", for: .normal)
+        }
+        }
+    }
+    
 
-}
 
